70d1ac1cc35e4dca1650455b728c7d700ea98bdb
serhii.zghama <serhii.zghama@edetek.com>
2024-03-04 14:42:02 +0700

    Finish implement models

--- models.py
+++ models.py
@@ -9,6 +9,17 @@ class Field:
         return str(self.value)
 
 
+class Name(Field):
+    def __init__(self, name):
+        super().__init__(name)
+
+    def __hash__(self):
+        return hash(self.value)
+
+    def __eq__(self, other):
+        return self.value == other.value
+
+
 class Phone(Field):
     def __init__(self, value):
         if not value.isdigit() or len(value) != 10:
@@ -18,7 +29,7 @@ class Phone(Field):
 
 class Record:
     def __init__(self, name):
-        self.name = name
+        self.name = Name(name)
         self.phones = []
 
     def add_phone(self, phone):
@@ -54,4 +65,35 @@ class AddressBook(UserDict):
 
 
 if __name__ == "__main__":
+    # Створення нової адресної книги
     book = AddressBook()
+
+    # Створення запису для John
+    john_record = Record("John")
+    john_record.add_phone("1234567890")
+    john_record.add_phone("5555555555")
+
+    # Додавання запису John до адресної книги
+    book.add_record(john_record)
+
+    # Створення та додавання нового запису для Jane
+    jane_record = Record("Jane")
+    jane_record.add_phone("9876543210")
+    book.add_record(jane_record)
+
+    # Виведення всіх записів у книзі
+    for name, record in book.data.items():
+        print(record)
+
+    # Знаходження та редагування телефону для John
+    john = book.find("John")
+    john.edit_phone("1234567890", "1112223333")
+
+    print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+    # Пошук конкретного телефону у записі John
+    found_phone = john.find_phone("5555555555")
+    print(f"{john.name}: {found_phone}")  # Виведення: 5555555555
+
+    # Видалення запису Jane
+    book.delete("Jane")
