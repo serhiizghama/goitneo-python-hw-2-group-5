30657532c79d92cafea2bebb199bcc0cff7e7827
serhii.zghama <serhii.zghama@edetek.com>
2024-03-04 14:38:20 +0700

    Implement models

--- models.py
+++ models.py
@@ -1,1 +1,57 @@
-print("Hello")
+from collections import UserDict
+
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+
+class Phone(Field):
+    def __init__(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Invalid phone number format")
+        super().__init__(value)
+
+
+class Record:
+    def __init__(self, name):
+        self.name = name
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        if phone in self.phones:
+            self.phones.remove(phone)
+
+    def edit_phone(self, old_phone_number, new_phone_number):
+        for phone in self.phones:
+            if phone.value == old_phone_number:
+                phone.value = new_phone_number
+                break
+
+    def find_phone(self, phone):
+        return [p.value for p in self.phones if p.value == phone]
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(str(p) for p in self.phones)}"
+
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        return self.data.get(name)
+
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+
+
+if __name__ == "__main__":
+    book = AddressBook()
