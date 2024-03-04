822ecbad77271cf8d99c8f9d414eddc974e70fe5
serhii.zghama <serhii.zghama@edetek.com>
2024-03-04 14:30:09 +0700

    Implement input_error

--- main.py
+++ main.py
@@ -1,35 +1,64 @@
 contacts = {}
 
-def parse_input(user_input):
-    cmd, *args = user_input.split()
-    cmd = cmd.strip().lower()
-    return cmd, *args
+
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except ValueError:
+            return "Give me name and phone please."
+        except KeyError:
+            return "Contact not found."
+        except IndexError:
+            return "Invalid command format."
+        except Exception as e:
+            return str(e)
+    return inner
 
 
+@input_error
+def hello():
+    return "How can I help you?"
+
+
+@input_error
 def add_contact(args, contacts):
     name, phone = args
     contacts[name] = phone
     return "Contact added."
 
 
+@input_error
 def change_contact(args, contacts):
-    name, new_phone = args
+    name, phone = args
     if name in contacts:
-        contacts[name] = new_phone
+        contacts[name] = phone
         return "Contact updated."
     else:
-        return "Contact not found."
+        raise KeyError
 
 
+@input_error
 def show_phone(args, contacts):
-    name = args[0]
-    if name in contacts:
-        return contacts[name]
-    else:
-        return "Contact not found."
+    name, = args
+    return f"Phone number for {name}: {contacts[name]}"
 
-def show_all():
-   print(contacts)
+
+@input_error
+def show_all(args, contacts):
+    if contacts:
+        result = "All contacts:\n"
+        for name, phone in contacts.items():
+            result += f"{name}: {phone}\n"
+        return result.strip()
+    else:
+        raise KeyError("No contacts found.")
+
+
+def parse_input(user_input):
+    cmd, *args = user_input.split()
+    cmd = cmd.strip().lower()
+    return cmd, *args
 
 
 def main():
